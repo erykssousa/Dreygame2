89a4725b040376a2222782a7d9035df049f7dcdf
erykssousa <130868567+erykssousa@users.noreply.github.com>
2025-10-07 13:47:55 +0100

    Update h2h.js

--- h2h.js
+++ h2h.js
@@ -95,7 +95,7 @@ function showRanking() {
       // ---------- pede ranking global ----------
       const cb2 = 'global' + Date.now();
       const script2 = document.createElement('script');
-      script2.src = `https://script.google.com/macros/s/AKfycbywYVEJtimYiP3HAzq8Ad9Y7JUmLeW3kBqiKfw4pewkbsU3fRumLlsbfkbPaY81XJxtsQ/exec?ranking=1&callback=${cb2}`;
+      script2.src = `https://script.google.com/macros/s/AKfycbywYVEJtimYiP3HAzq8Ad9Y7JUmLeW3kBqiKfw4pewkbsU3fRumLlsbfkbPaY81XJxtsQ/exec?ranking=1&callback=${cb2}`; 
       window[cb2] = function (top) {
       console.log('Ranking recebido:', top);
       let tbl = '<h3>Ranking Global (Top 30)</h3><ol>';
@@ -133,6 +133,7 @@ function resetGame(){
   location.reload();
 }
 render();
+
 
 
 
